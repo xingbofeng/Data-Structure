c4b20b1e315dc05cbdfca92441995bdfd78b2eb2
FE_XingBofeng <encounterxingbofeng@outlook.com>
2017-01-30 12:53:28 +0800

    fix: 3.2.5

--- ch3/example3.2.5.js
+++ ch3/example3.2.5.js
@@ -1,11 +1,11 @@
 // 例题3.2.5，表达式求值
 const symbols = {
-	'*': 4,
+	'*': 3,
 	'/': 3,
-	'+': 3,
+	'+': 2,
 	'-': 2,
 	'(': 1,
-	')': 6,
+	')': 4,
 	'#': 0,
 };
 // 运算的函数
@@ -42,17 +42,45 @@ function evaluation(str) {
 				} else if ((symbols[arr[i]] === ')') && (symbolstack[symbolstack.length - 1] === '(')) {
 					// 如果栈顶是'('并且读到了')'，则脱括号
 					symbolstack.pop();
-				} else if (symbols[arr[i]] < symbols[symbolstack[symbolstack.length - 1]]) {
+				} else if (symbols[arr[i]] <= symbols[symbolstack[symbolstack.length - 1]]) {
 					// 如果栈顶符号优先级高，则拿出栈顶符号进行运算
 					right = numstack.pop();
 					left = numstack.pop();
 					numstack.push(operation(left, right, symbolstack.pop()));
-					symbolstack.push(arr[i]);
+					// 此步进行运算但是并不读符号，因为我们已经把数字推入操作数栈
+					// i-- 则下次还是读这个符号，直到清空栈里的操作数只剩下一个'#'
+					// 此刻操作数栈优先级低，就不会走这个else if了
+					i--;
 				}
 			}
 		}
 	}
-	console.log(numstack);
 	console.log(symbolstack);
+	console.log(numstack);
+	// var i = 0;
+	// while (arr[i] !== '#') {
+	// 	if (/\d/.test(arr[i])) {
+	// 		numstack.push(parseInt(arr[i], 10));
+	// 		i++;
+	// 	} else {
+	// 		if (symbols[arr[i]] > symbols[symbolstack[symbolstack.length - 1]]) {
+	// 			// 如果栈顶符号优先级低，则入栈
+	// 			symbolstack.push(arr[i]);
+	// 			i++;
+	// 		} else if ((symbols[arr[i]] === ')') && (symbolstack[symbolstack.length - 1] === '(')) {
+	// 			// 如果栈顶是'('并且读到了')'，则脱括号
+	// 			symbolstack.pop();
+	// 			i++;
+	// 		} else if (symbols[arr[i]] <= symbols[symbolstack[symbolstack.length - 1]]) {
+	// 			// 如果栈顶符号优先级高，则拿出栈顶符号进行运算
+	// 			right = numstack.pop();
+	// 			left = numstack.pop();
+	// 			numstack.push(operation(left, right, symbolstack.pop()));
+	// 			symbolstack.push(arr[i]);
+	// 			i++;
+	// 		}
+	// 	}
+	// }
+	// console.log(numstack);
 }
-evaluation('1+2*3/4#');
+evaluation('1+3+3+2*3/4+3+3+3/2#');
