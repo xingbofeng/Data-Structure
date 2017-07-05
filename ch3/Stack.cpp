bc0cad5354e7f8732fe4781bc3140b1acbbcc926
xingbofeng <503908971@qq.com>
2017-07-05 23:17:38 +0800

    栈

--- ch3/Stack.cpp
+++ ch3/Stack.cpp
@@ -5,56 +5,43 @@
 #include <stdlib.h>
 
 struct stack {
-  int* base; // 栈底指针
-  int* top; // 栈顶指针
-  int stacksize; // 栈大小，多少个sizeof struct stack*
+  int* stackItem; // 数据域
+  int top; // 栈顶
+  int stackSize; // 栈大小
 };
 
-struct stack* init() {
-  // 初始化一个栈
-  struct stack* stack;
-  stack = (struct stack*)malloc(sizeof(struct stack*));
-  int* p = NULL; // 声明一个指针，指向栈顶也指向栈底
-  // p = (int*)malloc(sizeof(int*));
-  // 为栈分配指定的内存
-  stack->base = p;
-  stack->top = p;
-  stack->stacksize = 0;
+struct stack* init(struct stack* stack, int maxN) {
+  stack->top = -1;
+  // 动态生成数组空间
+  stack->stackItem = (int*)malloc(maxN * sizeof(int*));
+  stack->stackSize = maxN;
   return stack;
 }
 
 void printStack(struct stack* stack) {
-  // int* p;
-  // p = (int*)malloc(sizeof(int*));
-  // p = stack->base;
-  // while (p) {
-  //   printf("%d", *p);
-  //   p++;
-  // }
-  for (int i = 0; i < stack->stacksize; ++i) {
-    printf("%d", *stack->base);
-    stack->base++;
+  for (int i = 0; i <= stack->top; ++i) {
+    printf("%d", stack->stackItem[i]);
   }
 }
 
 void push(struct stack* stack, int data) {
-  // 如果栈满，realloc内存
-  if (stack->top - stack->base >= stack->stacksize) {
-    stack->base = (int*)realloc(stack->base, (stack->stacksize + 1) * sizeof(int*));
+  if (stack->stackSize == stack->top) {
+    stack->stackItem = (int*)realloc(stack->stackItem, (++(stack->top)) * sizeof(int*));
+    stack->stackItem[stack->top] = data;
+    stack->stackSize++;
+  } else {
+    stack->stackItem[++(stack->top)] = data;
   }
-  stack->top++;
-  stack->stacksize++;
-  stack->top = &data;
-  // int* p = (int*)malloc(sizeof(int*));
 }
 
 int main() {
-  struct stack* stack;
-  stack = (struct stack*)malloc(sizeof(struct stack*));
-  stack = init();
+  struct stack* stack = (struct stack*)malloc(sizeof(struct stack*));
+  int number;
+  scanf("%d", &number);
+  stack = init(stack, number);
   printStack(stack);
+  push(stack, 1);
   push(stack, 2);
-  // printf("%d", *stack->top);
   printStack(stack);
   return 0;
 }
